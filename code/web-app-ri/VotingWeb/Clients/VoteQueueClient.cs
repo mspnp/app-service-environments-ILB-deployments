78b4a001e6734441fbf0351f6032147ffdb179f6
Federico Arambarri <v-federicoar@microsoft.com>
2025-09-10 16:02:08 -0300

    Log

--- code/web-app-ri/VotingWeb/Clients/VoteQueueClient.cs
+++ code/web-app-ri/VotingWeb/Clients/VoteQueueClient.cs
@@ -5,11 +5,10 @@
 
 using Azure.Messaging.ServiceBus;
 using Microsoft.Extensions.Configuration;
-using System;
+using Microsoft.Extensions.Logging;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
-using VotingWeb.Exceptions;
 using VotingWeb.Interfaces;
 
 namespace VotingWeb.Clients
@@ -18,29 +17,26 @@ namespace VotingWeb.Clients
     {
         private readonly ServiceBusSender _sender;
 
-        public VoteQueueClient(ServiceBusClient client, IConfiguration config)
+        private readonly ILogger<VoteQueueClient> _logger;
+
+        public VoteQueueClient(ServiceBusClient client, IConfiguration config, ILogger<VoteQueueClient> logger)
         {
             var queueName = config["ConnectionStrings:queueName"];
+            var sbnamespace = config["ConnectionStrings:sbNamespace"];
+            logger.LogInformation("Queue {Queue} {Namespace}" , queueName, sbnamespace);
             _sender = client.CreateSender(queueName);
+            _logger = logger;
         }
 
         public async Task SendVoteAsync(int id)
         {
             var messageBody = new { Id = id };
-            try
+            var message = new ServiceBusMessage(Encoding.UTF8.GetBytes(JsonSerializer.Serialize(messageBody)))
             {
-                var message = new ServiceBusMessage(Encoding.UTF8.GetBytes(JsonSerializer.Serialize(messageBody)))
-                {
-                    ContentType = "application/json",
-                };
-                await _sender.SendMessageAsync(message);
-            }
-            catch (Exception ex) when (ex is ArgumentException ||
-                                               ex is UnauthorizedAccessException ||
-                                               ex is ServiceBusException)
-            {
-                throw new VoteQueueException("Service Bus Exception occurred with sending message to queue", ex);
-            }
+                ContentType = "application/json",
+            };
+            _logger.LogInformation("Message {QueueMessage}", JsonSerializer.Serialize(messageBody));
+            await _sender.SendMessageAsync(message);
         }
     }
 }
